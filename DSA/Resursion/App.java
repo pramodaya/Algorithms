2d9bf08a2a9f8079d4bcf1913636a904b396094a
pramodaya <pramodayajayalath@gmail.com>
2019-02-23 23:01:32 +0530

    Create App.java
    Main function

---
+++ DSA/Resursion/App.java
@@ -0,0 +1,10 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		int [] array = {1,38,5,8,10,30,40};
+		Binary obj = new Binary(array);
+		System.out.println(obj.binarySearch(40));
+	}
+
+}
